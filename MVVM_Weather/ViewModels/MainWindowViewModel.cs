f1b95393adc33210d0eaf3803d212022a71652b5
--- <mail@mail.com>
2023-04-03 15:48:32 +0300

    Добавьте файлы проекта.

---
+++ MVVM_Weather/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MVVM_Weather.VIewModels;
+
+public class MainWindowViewModel : BaseViewModel
+{
+    private string _title;
+
+    public string Title
+    {
+        get => _title; 
+        set => SetField(ref _title, value); 
+    }
+}
